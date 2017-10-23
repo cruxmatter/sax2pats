9c64cd8765c688f28ab7591d56f440dee84055bf
Alexander Smith <saalexantay@gmail.com>
2017-10-23 15:29:59 -0500

    remove claim type for now

--- lib/sax2pats/claim.rb
+++ lib/sax2pats/claim.rb
@@ -1,15 +1,11 @@
 module Sax2pats
   class Claim
     include Entity
-    attr_accessor :refs, :text_elements, :claim_id, :type
+    attr_accessor :refs, :text_elements, :claim_id
 
     def initialize
       @refs = []
       @text_elements = []
-    end
-
-    def type
-      @type ||= @refs.count > 0 ? :dependent : :independent
     end
 
     def as_text
