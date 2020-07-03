4b696da883baa5495f3573c7c05492bfd6521f95
Alex Smith <saalexantay@gmail.com>
2020-07-03 12:26:09 -0400

    Restore test file and use before each

--- spec/classification_loader_spec.rb
+++ spec/classification_loader_spec.rb
@@ -3,9 +3,7 @@
 require 'spec_helper'
 
 describe 'CPC::Loader' do
-  before(:all) do
-    @spec_data_path = ['spec', 'classification_data']
-  end
+  let(:spec_data_path) { ['spec', 'classification_data'] }
   let(:redis_host) { nil }
   let(:redis_port) { nil }
   let(:redis_password) { nil }
@@ -14,7 +12,7 @@ describe 'CPC::Loader' do
       redis_host: redis_host,
       redis_port: redis_port,
       redis_password: redis_password,
-      data_path: @spec_data_path
+      data_path: spec_data_path
     ) 
   end
 
@@ -23,22 +21,22 @@ describe 'CPC::Loader' do
   end
 
   context 'versions processed' do
-    before(:all) do
-      @loader = Sax2pats::CPC::Loader.new(
-        data_path: @spec_data_path
-      )
-      @loader.process_all_versions!
+    
+    before do
+      loader.process_all_versions!
     end
 
-    after(:all) { @loader.clear_data! }
+    after do 
+      loader.clear_data!
+    end
 
-    it { expect(@loader.loaded?).to eq true }
+    it { expect(loader.loaded?).to eq true }
 
     it 'keys exist and are accessible' do
-      expect(@loader.title('A01B63/22', cpc_release_date: '201309').fetch('parent'))
+      expect(loader.title('A01B63/22', cpc_release_date: '201309').fetch('parent'))
         .to eq 'A01B63/16'
     end
 
-    it { expect { @loader.clear_data! }.to change { @loader.key_size }.to(0) }
+    it { expect { loader.clear_data! }.to change { loader.key_size }.to(0) }
   end
 end
